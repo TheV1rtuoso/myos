1247539b36ed9fcce3a3f87c2f1274cd4d82d64e
TheV1rtuoso <simonhasir@gmail.com>
2024-04-03 00:38:09 +0200

    Kernel: TODO check memset, i get a weird crash when the compiler optimized to memset, Add partial solution

--- libc/string/memset.cpp
+++ libc/string/memset.cpp
@@ -1,9 +1,14 @@
 #include <string.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <assert.h>
 
 void *memset(void *bufptr, int value, size_t size)
 {
-    unsigned char *buf = (unsigned char *)bufptr;
-    for (size_t i = 0; i < size; i++)
-        buf[i] = (unsigned char)value;
+    assert(size % 4 == 0);
+    auto ptr = static_cast<uint32_t *>(bufptr);
+    for (size_t i = 0; i < size / 4; ++i) {
+        ptr[i] = value;
+    }
     return bufptr;
 }
